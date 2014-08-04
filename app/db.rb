9d7777b4a2877715ebe213ed38aa32fd230d47c6
btmeme <btmeme@rit.edu>
2014-08-04 12:55:24 -0400

    Refactored DB for after Wire refactor

--- app/db.rb
+++ app/db.rb
@@ -1,19 +1,26 @@
 require_relative '../wire'
 
+class Wire
+	
+	module App
+		def db_setup( namespace , location )
+			$config[@currentURI][:db_namespace] = namespace
+			$config[@currentURI][:db_location] = location
+			DataMapper.setup( namespace , location )
+		end
+	end
+
+	module Resource
+		def db_model( model )
+			$config[@currentURI][:resources][@currentResource][:db_model] = model
+		end
+	end
+
+end
+
 class DB
 
 	class Resource
-		include Wire::Resource
-
-		def initialize( uri , parentURI )
-			super( uri, parentURI )
-
-
-		@create[""] = Proc.new do
-			hash = request.body
-			hash = @model.flatten( hash )
-			@model.create( hash )
-		end
 
 		def readAll( params )
 			items = @model.all
@@ -28,83 +35,15 @@ class DB
 			@model.inflate( hash )
 		end
 
-		@update["/:id"] = Proc.new do
-			hash = request.body
-			hash = @model.flatten( hash )
-			hash = @model.update( hash )
-			@model.inflate( hash )
-		end
-
-		@delete["/id"] = Proc.new do
-			hash = request.body
-			@model.delete( hash )
-		end
-
-		end
-
-		def enableRead
-			read_all = Proc.new do
-				puts "Namespace: #{@namespace}"
-				DataMapper.repository(@namespace) do
-					@model.all
-				end
-			end
-                        Web.instance.sinatra.get(@uri) do
-				puts self.instance_variables
-				puts request.path
-				puts $resources[request.path].class
-				$resources[request.path].instance_eval( &read_all )
-			end
-		end
-
-		def enableUpdate
-
-		end
-
-		def enableDelete
-
-		end
-
-		def db_model( klass )
-			@model = klass
-		end
-
-		def db_namespace( ns )
-			@namespace = ns
-		end
-		
-		def info
-			super
-			puts "\t\tDB Model: #{@model}"
-			puts "\n"
-		end
 	end
 
 	class App
 		include Wire::App
 
-		def builder
-			@type = 'DB'
+		def resource
 			DB::Resource
 		end
 
-		def db_setup( ns , server )
-			@namespace = ns
-			@server = server
-			DataMapper::Logger.new($stdout, :debug)
-			DataMapper.setup( @namespace , @server )
-		end
-
-		def finish( uri )
-			@resources[uri].db_namespace( @namespace )
-		end
-
-		def info
-			super
-			puts "\tDB Namespace: #{@namespace}"
-			puts "\tDB Server: \'#{@server}\'"
-			puts "\n"
-		end
 	end
 
 end
