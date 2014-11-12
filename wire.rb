d6cca7b036f45fc7848f2d1007a17c47e6d50217
Bryan T. Meyers <btmeme@rit.edu>
2014-11-11 20:00:19 -0500

    CLEANED: Formatting of strings and hashes to match Ruby 1.9+

--- wire.rb
+++ wire.rb
@@ -21,7 +21,7 @@ class Sinatra::Base
 			user = 'nobody'
 			flag = 'false'
 		end
-		hash = {:failure => false}
+		hash = {failure: false}
 		hash[:sinatra] = self
 		hash[:user] = user
 		app = $config[:apps][appName]
@@ -36,14 +36,14 @@ class Sinatra::Base
 			if( type != nil ) then
 				hash[:controller] = type
 			else
-				hash[:message] = "Application type Not Specified"
+				hash[:message] = 'Application type Not Specified'
 				hash[:failure] = true
 			end
 		else
-			hash[:message] = "App Undefined"
+			hash[:message] = 'App Undefined'
 			hash[:failure] = true
 		end
-		page = ""
+		page = ''
 		if( id != nil ) then
 			if( id.include?('.') ) then
 				page = id.slice(0..(id.index('.')-1))
@@ -61,10 +61,10 @@ class Wire
 
 		def app( baseURI , &block)
 			@currentURI = baseURI
-			$config[:apps][baseURI] = {:resources => {}}
+			$config[:apps][baseURI] = {resources: {}}
 			@currentApp = $config[:apps][baseURI]
 			puts "Starting App at: /#{baseURI}"
-			puts "Setting up resources..."
+			puts 'Setting up resources...'
 			Docile.dsl_eval( self, &block )
 		end
 
@@ -73,23 +73,23 @@ class Wire
 		end
 
 		def create( context , request , response )
-			"Action not allowed"
+			'Action not allowed'
 		end
 
 		def readAll( context , request , response )
-			"Action not allowed"
+			'Action not allowed'
 		end
 
 		def read( id , context , request , response )
-			"Action not allowed"
+			'Action not allowed'
 		end
 
 		def update( id , context , request , response )
-			"Action not allowed"
+			'Action not allowed'
 		end
 
 		def delete( id , context , request , response )
-			"Action not allowed"
+			'Action not allowed'
 		end
 	end
 
@@ -100,7 +100,7 @@ class Wire
 		end
 
 		def auth_level( level )
-			@currentApp[:auth] = { :level => level }
+			@currentApp[:auth] = { level: level }
 		end
 		
 		def auth_user( user )
@@ -130,30 +130,29 @@ class Wire
 			@sinatra = Sinatra.new
 
 			## Create One or More
-			@sinatra.put("/:app/:resource") do | a , r |
+			@sinatra.put('/:app/:resource') do | a , r |
 				user = headers[:from]
 				context = prepare( a , r , user , r )
 				if( !context[:failure] ) then
 					if( actionAllowed?( :create , a , r , nil , user ) ) then
 						context[:controller].create( context , request , response )
 					else
-						"Operation not allowed"
+						'Operation not allowed'
 					end
-
 				else
 					context[:message]
 				end
 			end
 
 			## Read all
-			@sinatra.get("/:app/:resource") do | a , r |
+			@sinatra.get('/:app/:resource') do | a , r |
 				user = headers[:from]
 				context = prepare( a , r , user , r)
 				if( !context[:failure] ) then
 					if( actionAllowed?( :readAll , a , r , nil , user ) ) then
 						context[:controller].readAll( context , request , response )
 					else
-						"Operation not allowed"
+						'Operation not allowed'
 					end
 				else
 					context[:message]
@@ -161,14 +160,14 @@ class Wire
 			end
 
 			## Read One
-			@sinatra.get("/:app/:resource/*") do | a , r , i |
+			@sinatra.get('/:app/:resource/*') do | a , r , i |
 				user = headers[:from]
 				context = prepare( a , r , user, i)
 				if( !context[:failure] ) then
 					if( actionAllowed?( :read , a , r , i , user ) ) then
 						context[:controller].read( i , context , request , response )
 					else
-						"Operation not allowed"
+						'Operation not allowed'
 					end
 				else
 					context[:message]
@@ -176,14 +175,14 @@ class Wire
 			end
 
 			## Update One or More
-			@sinatra.post("/:app/:resource/*" ) do | a , r , i |
+			@sinatra.post('/:app/:resource/*' ) do | a , r , i |
 				user = headers[:from]
 				context = prepare( a , r , user , i)
 				if( !context[:failure] ) then
 					if( actionAllowed?( :update , a , r , i , user ) ) then
 						context[:controller].update( i , context , request , response )
 					else
-						"Operation not allowed"
+						'Operation not allowed'
 					end
 				else
 					context[:message]
@@ -191,14 +190,14 @@ class Wire
 			end
 
 			## Delete One
-			@sinatra.delete("/:app/:resource/*") do | a , r , i |
+			@sinatra.delete('/:app/:resource/*') do | a , r , i |
 				user = headers[:from]
 				context = prepare( a , r , user , i)
 				if( !context[:failure] ) then
-					if( $auth.delete?( :delete , a , r , i , user ) ) then
+					if( actionAllowed?( :delete , a , r , i , user ) ) then
 						context[:controller].delete( i , context , request , response )
 					else
-						"Operation not permitted"
+						'Operation not permitted'
 					end
 				else
 					context[:message]
@@ -209,8 +208,8 @@ class Wire
 		end
 
 		def build( &block )
-			puts "Starting Up Wire..."
-			puts "Starting Apps..."
+			puts 'Starting Up Wire...'
+			puts 'Starting Apps...'
 			Docile.dsl_eval( self , &block )
 		end
 
