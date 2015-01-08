820338175dee8dd98e4aebc7ecf3aa1f8fd4366f
Bryan T. Meyers <btmeme@rit.edu>
2015-01-08 10:00:53 -0500

    FIXED: Minor CSS Annoyances UPDATED: Table styling ADDED: Interwiki link translation for MediaWiki

--- app/repo.rb
+++ app/repo.rb
@@ -41,89 +41,70 @@ class Repo
     def self.readAll( context , request , response )
       context[:sinatra].pass unless (context[:resource_name] != nil )
       path = context[:resource_name]
-      if( path != nil ) then
-        referrer = request[:referrer]
-        puts referrer
-        info = `svn info --xml https://kgcoe-research.rit.edu/dav/#{path}`
-        unless $?.success? then
-          500
-        end
-        info = $nori.parse( info )
-        mime = 'text/html'
-        list = `svn --xml list https://kgcoe-research.rit.edu/dav/#{path}`
-        unless $?.success? then
-          500
-        end
-        list = $nori.parse( list )
-        ap list
-        referrer = request.env['HTTP_REFERRER']
-        unless referrer.nil? then
-          referrer = referrer.split('/')[3]
-        else
-          referrer = request.url.split('/')[3]
-        end
-        template = Tilt.new( 'views/lists/dav.haml' , 1 , {ugly: true})
-        list = template.render( self, list: list[:lists][:list][:entry], resource: path , id: '',  referrer: referrer)
-        body = list
-        response.headers['Content-Type'] = mime
-        response.headers['Cache-Control'] = 'public'
-        response.headers['Expires'] = "#{(Time.now + 1000).utc}"
-        response.body = body
-      else
-        'Root Directory not specified'
+      referrer = request.env['HTTP_REFERRER']
+      mime = 'text/html'
+      list = `svn --xml list https://kgcoe-research.rit.edu/dav/#{path}`
+      unless $?.success? then
+        500
       end
+      list = $nori.parse( list )
+      unless referrer.nil? then
+        referrer = referrer.split('/')[3]
+      else
+        referrer = request.url.split('/')[3]
+      end
+      template = Tilt.new( 'views/lists/dav.haml' , 1 , {ugly: true})
+      list = template.render( self, list: list[:lists][:list][:entry], resource: path , id: '',  referrer: referrer)
+      response.headers['Content-Type'] = mime
+      response.headers['Cache-Control'] = 'public'
+      response.headers['Expires'] = "#{(Time.now + 1000).utc}"
+      response.body = list
     end
 
     def self.read( id , context , request , response )
       context[:sinatra].pass unless (context[:resource_name] != nil )
       path = context[:resource_name]
-      referrer = request[:referrer]
-      puts referrer
-      if( path != nil ) then
-        info = `svn info --xml https://kgcoe-research.rit.edu/dav/#{path}/#{id}`
-        unless $?.success? then
-          response.headers['Content-Type'] = 'text/html'
-          response.body = Tilt.new( 'views/forms/new.haml').render
-          return response
-        end
-        info = $nori.parse( info )
-        type = info[:info][:entry][:@kind]
-        if type.eql? 'dir' then
-          mime = 'text/html'
-          list = `svn --xml list https://kgcoe-research.rit.edu/dav/#{path}/#{id}`
-          unless $?.success? then
-            500
-          end
-          list = $nori.parse( list )
-          referrer = request.env['HTTP_REFERRER']
-          referrer = referrer.split('/')[3]
-          template = Tilt.new( 'views/lists/dav.haml' , 1 , {ugly: true})
-          list = template.render( self, list: list[:lists][:list][:entry], resource: path , id: id, referrer: referrer)
-          body = list
-        else
-          body = `svn cat https://kgcoe-research.rit.edu/dav/#{path}/#{id}`
-          unless $?.success? then
-            Tilt.new( 'views/forms/new.haml').render
-          end
-          mime = `svn --xml propget svn:mime-type https://kgcoe-research.rit.edu/dav/#{path}/#{id}`
-          unless $?.success? then
-            500
-          end
-          mime = $nori.parse( mime )
-          ap mime
-          unless mime[:properties].nil? then
-            mime = mime[:properties][:target][:property]
-          else
-            mime = 'application/octet-stream'
-          end
-        end
-        response.headers['Content-Type'] = mime
-        response.headers['Cache-Control'] = 'public'
-        response.headers['Expires'] = "#{(Time.now + 1000).utc}"
-        response.body = body
-      else
-        404
+      referrer = request.env['HTTP_REFERRER']
+      ap referrer
+      info = `svn info --xml https://kgcoe-research.rit.edu/dav/#{path}/#{id}`
+      unless $?.success? then
+        response.headers['Content-Type'] = 'text/html'
+        response.body = Tilt.new( 'views/forms/new.haml').render(self, resource: path, id: id)
+        response.status = 200
+        return response
       end
+      info = $nori.parse( info )
+      type = info[:info][:entry][:@kind]
+      if type.eql? 'dir' then
+        mime = 'text/html'
+        list = `svn --xml list https://kgcoe-research.rit.edu/dav/#{path}/#{id}`
+        unless $?.success? then
+          500
+        end
+        list = $nori.parse( list )
+        referrer = referrer.split('/')[3]
+        template = Tilt.new( 'views/lists/dav.haml' , 1 , {ugly: true})
+        body = template.render( self, list: list[:lists][:list][:entry], resource: path , id: id, referrer: referrer)
+      else
+        body = `svn cat https://kgcoe-research.rit.edu/dav/#{path}/#{id}`
+        unless $?.success? then
+          500
+        end
+        mime = `svn --xml propget svn:mime-type https://kgcoe-research.rit.edu/dav/#{path}/#{id}`
+        unless $?.success? then
+          500
+        end
+        mime = $nori.parse( mime )
+        unless mime[:properties].nil? then
+          mime = mime[:properties][:target][:property]
+        else
+          mime = 'application/octet-stream'
+        end
+      end
+      response.headers['Content-Type'] = mime
+      response.headers['Cache-Control'] = 'public'
+      response.headers['Expires'] = "#{(Time.now + 1000).utc}"
+      response.body = body
     end
   end
 end
