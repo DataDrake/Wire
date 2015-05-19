00c2bdbe7d2e5eabfdb495881df54f725602e32d
Bryan T. Meyers <btmeme@rit.edu>
2015-05-19 07:12:36 -0400

    FIXED: function repos() was in the wrong scope

--- app/repo.rb
+++ app/repo.rb
@@ -1,19 +1,20 @@
 require 'awesome_print'
 require_relative '../wire'
+require_relative 'repo/svn'
 
 module Repo
   extend Wire::App
   extend Wire::Resource
 
-  def self.repos( path )
+  def repos( path )
     $currentApp[:repos_path] = path
   end
 
-  def self.listing( path )
+  def listing( path )
     $currentApp[:template] = Tilt.new( path , 1 , {ugly: true})
   end
 
-  def self.create( context , request , response )
+  def create( context , request , response )
     context[:sinatra].pass unless (context[:resource] != nil )
     path = context[:app][:repos_path]
     resource = context[:resource]
@@ -28,12 +29,13 @@ module Repo
     end
   end
 
-  def self.readAll( context , request , response )
+  def readAll( context , request , response )
     context[:sinatra].pass unless (context[:resource_name] != nil )
     resource = context[:resource_name]
     referrer = request.env['HTTP_REFERRER']
+    repos = context[:app][:repos_path]
     mime = 'text/html'
-    list = do_read_listing( resource )
+    list = do_read_listing( repos, resource )
     if list == 500 then
       return 500
     end
@@ -50,20 +52,22 @@ module Repo
     response.body = list
   end
 
-  def self.read( id , context , request , response )
+  def read( id , context , request , response )
     context[:sinatra].pass unless (context[:resource_name] != nil )
     path = context[:resource_name]
     referrer = request.env['HTTP_REFERRER']
-    info = do_read_info( path , id )
+    repos = context[:app][:repos_path]
+    info = do_read_info( repos, path , id )
     if info == 404 then
       response.headers['Content-Type'] = 'text/html'
       response.body = Tilt.new( 'views/forms/new.haml').render(self, resource: path, id: id)
       response.status = 200
+      return response
     end
     type = info[:@kind]
     if type.eql? 'dir' then
       mime = 'text/html'
-      list = do_read_listing( path , id)
+      list = do_read_listing( repos, path , id)
       unless referrer.nil? then
         referrer = referrer.split('/')[3]
       else
@@ -72,15 +76,20 @@ module Repo
       template =context[:app][:template]
       body = template.render( self, list: list, resource: path , id: id, referrer: referrer)
     else
-      body = `svn cat 'https://kgcoe-research.rit.edu/dav/#{path}/#{id}'`
+      body = do_read( repos, path , id )
       if body == 500
         return body
       end
-      mime = do_read_mime( path , id )
+      mime = do_read_mime( repos, path , id )
     end
     response.headers['Content-Type'] = mime
     response.headers['Cache-Control'] = 'public'
     response.headers['Expires'] = "#{(Time.now + 1000).utc}"
     response.body = body
+  end
+
+  def update( id, context, request , response )
+    ap context[:params]
+    200
   end
 end
