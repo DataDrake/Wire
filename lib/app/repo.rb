7e4495189cbef3f6d7942e2061907286b155353f
Bryan T. Meyers <btmeme@rit.edu>
2015-08-04 21:16:52 -0400

    CHANGED: Repo converted to new wire style

--- lib/app/repo.rb
+++ lib/app/repo.rb
@@ -6,75 +6,76 @@ require_relative 'repo/svn'
 module Repo
 
   def repos( path )
-    $currentApp[:repos_path] = path
+    $current_app[:repos_path] = path
   end
 
   def listing( path )
-    $currentApp[:template] = Tilt.new( path , 1 , {ugly: true})
+    $current_app[:template] = Tilt.new( path , 1 , {ugly: true})
   end
 
   def web_folder( path )
-    $currentApp[:web] = path
+    $current_app[:web] = path
   end
 
-  def do_create( context , request , response , actions)
-    context[:sinatra].pass unless (context[:resource_name] != nil )
+  def do_create( context )
+    return 404 unless context[:resource_name]
     path = context[:app][:repos_path]
     resource = context[:resource_name]
-    if( path != nil ) then
-      unless Dir.exist?( "#{path}/#{resource}" )
-        do_create_file( path, resource)
-      else
+    if path
+      if Dir.exist?("#{path}/#{resource}")
         401
+      else
+        do_create_file(path, resource)
       end
     else
-      'Repo Directory not specified'
+      400
     end
   end
 
-  def do_readAll( context , request , response , actions)
-    context[:sinatra].pass unless (context[:resource_name] != nil )
+  def do_read_all( context )
+    return 404 unless context[:resource_name]
     resource = context[:resource_name]
-    referrer = request.env['HTTP_REFERRER']
+    referrer = context[:request].env['HTTP_REFERRER']
     repos = context[:app][:repos_path]
     web = context[:app][:web]
     mime = 'text/html'
     list = do_read_listing( web, repos, resource )
-    if list == 404 then
+    if list == 404
       return 404
     end
-    unless referrer.nil? then
-      referrer = referrer.split('/')[3]
+    if referrer.nil?
+      referrer = context[:request].url.split('/')[3]
     else
-      referrer = request.url.split('/')[3]
+      referrer = referrer.split('/')[3]
     end
     template = context[:app][:template]
     list = template.render( self, list: list, resource: resource , id: '',  referrer: referrer)
-    response.headers['Content-Type'] = mime
-    response.headers['Cache-Control'] = 'public'
-    response.headers['Expires'] = "#{(Time.now + 1000).utc}"
-    response.body = list
+    headers = {}
+    headers['Content-Type'] = mime
+    headers['Cache-Control'] = 'public'
+    headers['Expires'] = "#{(Time.now + 1000).utc}"
+    [200, headers , [list]]
   end
 
-  def do_read( id , context , request , response , actions)
-    context[:sinatra].pass unless (context[:resource_name] != nil )
+  def do_read( context )
+    return 404 unless context[:resource_name]
     path = context[:resource_name]
-    referrer = request.env['HTTP_REFERRER']
+    referrer = context[:request].env['HTTP_REFERRER']
     repos = context[:app][:repos_path]
     web = context[:app][:web]
     rev = context[:query][:rev]
     info = do_read_info( rev, web, repos, path , id )
-    if info == 404 then
+    if info == 404
       return 404
     end
     type = info[:@kind]
-    if type.eql? 'dir' then
+    if type.eql? 'dir'
       mime = 'text/html'
       list = do_read_listing( web, repos, path , id)
-      unless referrer.nil? then
-        referrer = referrer.split('/')[3]
+      if referrer.nil?
+        referrer = context[:request].url.split('/')[3]
       else
-        referrer = request.url.split('/')[3]
+        referrer = referrer.split('/')[3]
       end
       template =context[:app][:template]
       body = template.render( self, list: list, resource: path , id: id, referrer: referrer)
@@ -85,26 +86,42 @@ module Repo
       end
       mime = do_read_mime( rev, web, repos, path , id )
     end
-    response.headers['Content-Type'] = mime
-    response.headers['Cache-Control'] = 'public'
-    response.headers['Expires'] = "#{(Time.now + 1000).utc}"
-    response.body = body
+    headers = {}
+    headers['Content-Type'] = mime
+    headers['Cache-Control'] = 'public'
+    headers['Expires'] = "#{(Time.now + 1000).utc}"
+    [200, headers , [body]]
   end
 
-  def do_update( id, context, request , response , actions )
-    context[:sinatra].pass unless (context[:resource_name] != nil )
+  def do_update( context )
+    return 404 unless context[:resource_name]
     path = context[:resource_name]
-    referrer = request.env['HTTP_REFERRER']
     repos = context[:app][:repos_path]
     web = context[:app][:web]
     content = context[:params]
-    if content.include? 'file'
-      file = content['file']['content'].match(/base64,(.*)/)[1]
+    if content[:file]
+      file = content[:file][:content].match(/base64,(.*)/)[1]
       file = Base64.decode64( file )
-      do_update_file( web , repos, path , id , file , content['message'] , content['file']['mime'], context[:user] )
+      do_update_file( web , repos, path , id , file , content[:message] , content[:file][:mime], context[:user] )
     else
-      do_update_file( web , repos, path , id , content['updated'], content['message'] , context[:query]['type'], context[:user] )
+      do_update_file( web , repos, path , id , content[:updated], content[:message] , context[:query]['type'], context[:user] )
     end
+  end
 
+  def invoke( actions , context )
+    case context[:action]
+      when :create
+        do_create( context )
+      when :read
+        if context[:uri][3]
+          do_read( context )
+        else
+          do_read_all( context )
+        end
+      when :update
+        do_update( context )
+      else
+        403
+    end
   end
 end
