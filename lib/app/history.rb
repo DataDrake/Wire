b84cbc7dd9581b4a1d6c7ee984d129a293c6b8da
Bryan T. Meyers <btmeme@rit.edu>
2015-08-04 21:05:37 -0400

    History ported to new wire style

--- lib/app/history.rb
+++ lib/app/history.rb
@@ -1,31 +1,31 @@
 require 'awesome_print'
+require 'tilt'
 require_relative '../wire'
 require_relative 'history/svn'
 
 module History
 
-  def repos( path )
-    $currentApp[:repos_path] = path
+  def self.repos( path )
+    $current_app[:repos_path] = path
   end
 
-  def log( path )
-    $currentApp[:template] = Tilt.new( path , 1 , {ugly: true})
+  def self.log( path )
+    $current_app[:template] = Tilt.new( path , 1 , {ugly: true})
   end
 
-  def web_folder( path )
-    $currentApp[:web] = path
+  def self.web_folder( path )
+    $current_app[:web] = path
   end
 
-  def do_read( id , context , request , response , actions )
-    context[:sinatra].pass unless (context[:resource_name] != nil )
+  def self.do_read( context )
+    return 404 unless context[:resource_name]
     resource = context[:resource_name]
-    referrer = request.env['HTTP_REFERRER']
+    referrer = context[:request].env['HTTP_REFERRER']
     web = context[:app][:web]
     list = get_log( web, resource , id )
     if list == 404
       return 404
     end
-    ap referrer
     if referrer.nil?
       referrer = request.url
     end
@@ -35,7 +35,12 @@ module History
     template.render( self, list: list, resource: resource , id: id, referrer: referrer)
   end
 
-  def do_read_all( context , request , response , actions )
-    do_read( nil , context, request , response , actions)
+  def self.invoke( actions , context )
+    case context[:action]
+      when :read
+        do_read( context )
+      else
+        403
+    end
   end
 end
