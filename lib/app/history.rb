94f9c475d3c37c74aef5758a76026b868d31b411
Bryan T. Meyers <btmeme@rit.edu>
2015-08-12 18:25:43 -0400

    ADDED: documentation for History app

--- lib/app/history.rb
+++ lib/app/history.rb
@@ -3,20 +3,34 @@ require 'tilt'
 require_relative '../wire'
 require_relative 'history/svn'
 
+# History is a Wire::App for accessing the history of versioned content
+# @author Bryan T. Meyers
 module History
 
+	# Select the location of the repositories
+	# @param [Symbol] path location of the repositories
+	# @return [void]
 	def repos(path)
 		$current_app[:repos_path] = path
 	end
 
+	# Select the render template for histories
+	# @param [Symbol] path location of the Tilt compatible template
+	# @return [void]
 	def log(path)
 		$current_app[:template] = Tilt.new(path, 1, { ugly: true })
 	end
 
+	# Select the sub-directory for web-serveable content
+	# @param [Symbol] path the sub-directory path
+	# @return [void]
 	def web_folder(path)
 		$current_app[:web] = path
 	end
 
+	# Get the history of a single file or directory
+	# @param [Hash] context the context for this request
+	# @return [Response] the history, or status code
 	def do_read(actions, context)
 		resource = context.uri[2]
 		web      = context.app[:web]
@@ -29,6 +43,10 @@ module History
 		template.render(self, actions: actions, context: context, list: list)
 	end
 
+	# Proxy method used when routing
+	# @param [Array] actions the allowed actions for this URI
+	# @param [Hash] context the context for this request
+	# @return [Response] a Rack Response triplet, or status code
 	def invoke(actions, context)
 		return 404 unless context.uri[2]
 		case context.action
