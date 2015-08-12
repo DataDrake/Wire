c042b7b2acf5474861a7ef32c79f5af1af38424d
Bryan T. Meyers <btmeme@rit.edu>
2015-08-12 18:20:42 -0400

    ADDED: documentation for Static app

--- lib/app/file.rb
+++ lib/app/file.rb
@@ -1,14 +1,23 @@
 require 'awesome_print'
 require_relative '../wire'
 
+# Static is a Wire::App for serving read-only, static files
+# @author Bryan T. Meyers
 module Static
 	extend Wire::App
 	extend Wire::Resource
 
+	# Map a file folder to a sub-URI
+	# @param [String] resource the sub-URI
+	# @param [Class] path the file folder
+	# @return [void]
 	def self.local(resource, path)
 		$current_app[:resources][resource] = { local: path }
 	end
 
+	# Get a file listing for this folder
+	# @param [Hash] context the context for this request
+	# @return [Response] a listing, or status code
 	def self.do_read_all(context)
 		path = context.resource[:local]
 		if path
@@ -23,6 +32,9 @@ module Static
 		end
 	end
 
+	# Get a file from this folder
+	# @param [Hash] context the context for this request
+	# @return [Response] a file, listing, or status code
 	def self.do_read(context)
 		path = context.resource[:local]
 		id   = context.uri[3..context.uri.length].join('/')
@@ -49,6 +61,10 @@ module Static
 		end
 	end
 
+	# Proxy method used when routing
+	# @param [Array] actions the allowed actions for this URI
+	# @param [Hash] context the context for this request
+	# @return [Response] a Rack Response triplet, or status code
 	def self.invoke(actions, context)
 		return 404 unless context.resource
 		case context.action
