0f589c7c97295b5fe1561fec214a0cb020a96527
Bryan T. Meyers <btmeme@rit.edu>
2015-08-12 19:12:22 -0400

    ADDED: documentation for Wire module

--- lib/wire.rb
+++ lib/wire.rb
@@ -8,12 +8,19 @@ require 'json'
 require 'rack'
 
 module JSON
+
+	# Force JSON.parse to symbolize names
+	# @param [String] source the raw JSON string
+	# @param [Hash] opts any further options for JSON.parse
+	# @return [Hash] the parsed JSON content
 	def self.parse_clean(source, opts = {})
 		opts[:symbolize_names] = true
 		parse(source, opts)
 	end
 end
 
+# Wire is an environment for quickly building REST services
+# @author Bryan T. Meyers
 module Wire
 	VERSION = '0.1.0'
 end
