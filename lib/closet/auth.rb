f78b4535897ec10464a6788b50011e3bfa8fec42
Bryan T. Meyers <btmeme@rit.edu>
2015-08-12 18:46:48 -0400

    ADDED: documentation for Auth module

--- lib/closet/auth.rb
+++ lib/closet/auth.rb
@@ -1,6 +1,12 @@
+
 module Wire
+	# Auth is a module for handling authorization
+	# @author Bryan T. Meyers
 	module Auth
 
+		# Get the allowed actions for the current URI
+		# @param [Hash] context the context for this request
+		# @return [Array] the allowed actions for this URI
 		def actions_allowed(context)
 			actions = []
 			app     = context.app
@@ -22,6 +28,10 @@ module Wire
 			actions
 		end
 
+		# Setup auth for an App
+		# @param [Symbol] level the type of authz to perform
+		# @param [Proc] block setup for the authz
+		# @return [void]
 		def auth(level, &block)
 			$current_app[:auth] = { level: level }
 			unless (level == :any) || (block.nil?)
@@ -29,10 +39,15 @@ module Wire
 			end
 		end
 
+		# Select handler for :app level of auth
+		# @param [Module] handler the type of authz to perform
+		# @return [void]
 		def handler(handler)
 			$current_app[:auth][:handler] = handler
 		end
 
+		# Select user for :user level of auth
+		# @return [void]
 		def user(user)
 			$current_app[:auth][:user] = user
 		end
