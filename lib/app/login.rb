ab420c04287e5ca09fdf1864d4e52a619ca5b98a
Bryan T. Meyers <btmeme@rit.edu>
2015-08-12 18:27:04 -0400

    ADDED: documentation for Login app

--- lib/app/login.rb
+++ lib/app/login.rb
@@ -1,5 +1,11 @@
+# Login is a Wire::App for forcing user logins
+# @author Bryan T. Meyers
 module Login
 
+	# Proxy method used when routing
+	# @param [Array] actions the allowed actions for this URI
+	# @param [Hash] context the context for this request
+	# @return [Response] a redirect message returning to the previous page
 	def self.invoke(actions, context)
 		referer = context.referer
 		[301, { 'Location' => referer }, ['Login Redirect']]
