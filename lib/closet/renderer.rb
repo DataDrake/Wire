460e7e1567834ccf3c2e68523e3e5836ed633c60
Bryan T. Meyers <btmeme@rit.edu>
2015-08-12 18:58:12 -0400

    ADDED: documentation for Renderer module

--- lib/closet/renderer.rb
+++ lib/closet/renderer.rb
@@ -1,12 +1,21 @@
 module Wire
+	# Renderer is a module for mapping mime to rendering templates
+	# @author Bryan T. Meyers
 	module Renderer
 
+		# Setup a Renderer
+		# @param [Class] klass the type of renderer to use
+		# @param [Proc] block for configuring this renderer
+		# @return [void]
 		def renderer(klass, &block)
 			$current_renderer = klass
 			$current_editor   = nil
 			Docile.dsl_eval(self, &block)
 		end
 
+		# Associate a MIME type to the current Renderer or Editor
+		# @param [String] mime the MIME type
+		# @return [void]
 		def mime(mime)
 			if $current_renderer
 				$renderers[mime]              = $current_renderer
@@ -17,16 +26,21 @@ module Wire
 			end
 		end
 
+		# Setup a new template
+		# @param [String] template the template for a renderer or editor
+		# @return [void]
 		def partial(template)
 			$current_template = Tilt.new(template, 1, { ugly: true })
 		end
 
+		# Setup an Editor
+		# @param [Class] editor the template for this Editor
+		# @param [Proc] block for configuring this Editor
+		# @return [void]
 		def editor(editor, &block)
 			$current_editor   = Tilt.new(editor, 1, { ugly: true })
 			$current_renderer = nil
 			Docile.dsl_eval(self, &block)
 		end
-
 	end
-
 end
