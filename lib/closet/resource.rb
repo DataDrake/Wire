48fca07458543ad252b32536d30839d66b75e3f8
Bryan T. Meyers <btmeme@rit.edu>
2015-08-12 19:00:02 -0400

    ADDED: documentation for Resource module

--- lib/closet/resource.rb
+++ lib/closet/resource.rb
@@ -1,5 +1,13 @@
 module Wire
+
+	# Resource is a DSL function for mapping sub-URI in Wire::App(s)
+	# @author Bryan T. Meyers
 	module Resource
+
+		# Setup a Renderer
+		# @param [String] uri the sub-URI
+		# @param [Proc] block for configuring this resource
+		# @return [void]
 		def resource(uri, &block)
 			$current_app[:resources][uri] = {}
 			$current_resource             = $current_app[:resources][uri]
