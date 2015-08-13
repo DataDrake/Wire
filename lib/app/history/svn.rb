7cd55e3f1e03ced552895b9e5756d3b1d806a54b
Bryan T. Meyers <btmeme@rit.edu>
2015-08-12 20:38:40 -0400

    ADDED: documentation for History::SVN app

--- lib/app/history/svn.rb
+++ lib/app/history/svn.rb
@@ -2,11 +2,18 @@ require_relative '../repo'
 require 'cobravsmongoose'
 
 module History
+	# History::SVN is a connector for viewing log information in SVN
+	# @author Bryan T. Meyers
 	module SVN
 		extend Wire::App
 		extend Wire::Resource
 		extend History
 
+		# Get the log information for any part of a Repo
+		# @param [String] web the web path of the repo
+		# @param [String] repo the name of the repository to access
+		# @param [String] id the sub-URI of the item to access
+		# @return [Hash] the history entries
 		def self.get_log(web, repo, id = nil)
 			if id.nil?
 				log = `svn log -v --xml 'svn://localhost/#{repo}'`
