2adec853a7701208915c362afe8140163ce45732
Bryan T. Meyers <btmeme@rit.edu>
2015-08-12 20:42:43 -0400

    ADDED: documentation for Render::Document app

--- lib/app/render/document.rb
+++ lib/app/render/document.rb
@@ -1,9 +1,16 @@
 require_relative '../render'
 
 module Render
+	# Document renders a file to an HTML representation
+	# @author Bryan T. Meyers
 	module Document
 		extend Render
 
+		# Renders a document or listing to HTML
+		# @param [Array] actions the actions allowed for this URI
+		# @param [Wire::Context] context the context for this request
+		# @param [Symbol] specific the type of read to perform
+		# @return [Response] a Rack Response triplet, or status code
 		def self.do_read(actions, context, specific)
 			begin
 				response = forward(specific, context)
@@ -20,6 +27,10 @@ module Render
 			end
 		end
 
+		# Proxy method used when routing
+		# @param [Array] actions the allowed actions for this URI
+		# @param [Hash] context the context for this request
+		# @return [Response] a Rack Response triplet, or status code
 		def self.invoke(actions, context)
 			case context.action
 				when :create
