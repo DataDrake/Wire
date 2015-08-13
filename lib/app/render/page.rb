bd0f5e1bf1799a9716b3f2e32f43d26546239376
Bryan T. Meyers <btmeme@rit.edu>
2015-08-12 20:50:41 -0400

    ADDED: documentation for Render::Page app

--- lib/app/render/page.rb
+++ lib/app/render/page.rb
@@ -1,10 +1,18 @@
 require_relative '../render'
 
 module Render
+	# Page builds the a page that is presented directly to a user
+	# @author Bryan T. Meyers
 	module Page
 		include Render
 		extend self
 
+		# Render a full template, handling the gathering of additional Sources
+		# @param [Array] actions the allowed actions for this URI
+		# @param [Hash] context the context for this request
+		# @param [Tilt::Template] template a pre-loaded Tilt template to render
+		# @param [String] content the content to render into the page
+		# @return [Response] a Rack Response triplet, or status code
 		def render_template(actions, context, template, content)
 			if template[:path]
 				hash = { actions: actions, context: context, content: content }
@@ -33,6 +41,11 @@ module Render
 			message
 		end
 
+		# Render a page to its final form
+		# @param [Array] actions the allowed actions for this URI
+		# @param [Hash] context the context for this request
+		# @param [Symbol] specific the kind of read to perform
+		# @return [Response] a Rack Response triplet, or status code
 		def do_read(actions, context, specific)
 			template = context.app[:template]
 			resource = context.uri[2]
@@ -54,6 +67,10 @@ module Render
 			message
 		end
 
+		# Proxy method used when routing
+		# @param [Array] actions the allowed actions for this URI
+		# @param [Hash] context the context for this request
+		# @return [Response] a Rack Response triplet, or status code
 		def self.invoke(actions, context)
 			case context.action
 				when :create
