d4616d7524cb83d326a96ba0b73da563187c7f62
Bryan T. Meyers <btmeme@rit.edu>
2015-08-17 01:40:11 -0400

    FIXED: vulnerability where svn credentials were being stored as a class variable

--- lib/app/repo/svn.rb
+++ lib/app/repo/svn.rb
@@ -39,14 +39,14 @@ module Repo
 		# @param [String] id the relative path to the file
 		# @return [String] the file
 		def self.do_read_file(rev, web, path, repo, id)
-			@options = "--username=#{$environment[:repos_user]} --password=#{$environment[:repos_password]}"
+			options = "--username=#{$environment[:repos_user]} --password=#{$environment[:repos_password]}"
 			if rev.nil?
 				rev = 'HEAD'
 			end
 			if web.nil?
-				body = `svn cat #{@options} -r #{rev} 'svn://localhost/#{repo}/#{id}'`
+				body = `svn cat #{options} -r #{rev} 'svn://localhost/#{repo}/#{id}'`
 			else
-				body = `svn cat #{@options} -r #{rev} 'svn://localhost/#{repo}/#{web}/#{id}'`
+				body = `svn cat #{options} -r #{rev} 'svn://localhost/#{repo}/#{web}/#{id}'`
 			end
 
 			if $?.success?
@@ -63,18 +63,18 @@ module Repo
 		# @param [String] id the relative path to the file
 		# @return [Array] the directory listing
 		def self.do_read_listing(web, path, repo, id = nil)
-			@options = "--username=#{$environment[:repos_user]} --password=#{$environment[:repos_password]}"
+			options = "--username=#{$environment[:repos_user]} --password=#{$environment[:repos_password]}"
 			if web.nil?
 				if id.nil?
-					list = `svn list #{@options} --xml 'svn://localhost/#{repo}'`
+					list = `svn list #{options} --xml 'svn://localhost/#{repo}'`
 				else
-					list = `svn list #{@options} --xml 'svn://localhost/#{repo}/#{id}'`
+					list = `svn list #{options} --xml 'svn://localhost/#{repo}/#{id}'`
 				end
 			else
 				if id.nil?
-					list = `svn list #{@options} --xml 'svn://localhost/#{repo}/#{web}'`
+					list = `svn list #{options} --xml 'svn://localhost/#{repo}/#{web}'`
 				else
-					list = `svn list #{@options} --xml 'svn://localhost/#{repo}/#{web}/#{id}'`
+					list = `svn list #{options} --xml 'svn://localhost/#{repo}/#{web}/#{id}'`
 				end
 			end
 			unless $?.exitstatus == 0
@@ -92,14 +92,14 @@ module Repo
 		# @param [String] id the relative path to the file
 		# @return [Hash] the metadata
 		def self.do_read_info(rev, web, path, repo, id)
-			@options = "--username=#{$environment[:repos_user]} --password=#{$environment[:repos_password]}"
+			options = "--username=#{$environment[:repos_user]} --password=#{$environment[:repos_password]}"
 			if rev.nil?
 				rev = 'HEAD'
 			end
 			if web.nil?
-				info = `svn info #{@options} -r #{rev} --xml 'svn://localhost/#{repo}/#{id}'`
+				info = `svn info #{options} -r #{rev} --xml 'svn://localhost/#{repo}/#{id}'`
 			else
-				info = `svn info #{@options} -r #{rev} --xml 'svn://localhost/#{repo}/#{web}/#{id}'`
+				info = `svn info #{options} -r #{rev} --xml 'svn://localhost/#{repo}/#{web}/#{id}'`
 			end
 
 			unless $?.exitstatus == 0
@@ -117,14 +117,14 @@ module Repo
 		# @param [String] id the relative path to the file
 		# @return [String] the MIME type
 		def self.do_read_mime(rev, web, path, repo, id)
-			@options = "--username=#{$environment[:repos_user]} --password=#{$environment[:repos_password]}"
+			options = "--username=#{$environment[:repos_user]} --password=#{$environment[:repos_password]}"
 			if rev.nil?
 				rev = 'HEAD'
 			end
 			if web.nil?
-				mime = `svn propget #{@options} -r #{rev} --xml svn:mime-type 'svn://localhost/#{repo}/#{id}'`
+				mime = `svn propget #{options} -r #{rev} --xml svn:mime-type 'svn://localhost/#{repo}/#{id}'`
 			else
-				mime = `svn propget #{@options} -r #{rev} --xml svn:mime-type 'svn://localhost/#{repo}/#{web}/#{id}'`
+				mime = `svn propget #{options} -r #{rev} --xml svn:mime-type 'svn://localhost/#{repo}/#{web}/#{id}'`
 			end
 
 			unless $?.success?
@@ -149,9 +149,9 @@ module Repo
 		# @param [String] user the Author of this change
 		# @return [Integer] status code
 		def self.do_update_file(web, path, repo, id, content, message, mime, user)
-			@options = "--username=#{$environment[:repos_user]} --password=#{$environment[:repos_password]}"
+			options = "--username=#{$environment[:repos_user]} --password=#{$environment[:repos_password]}"
 			status   = 500
-			`svn checkout #{@options} svn://localhost/#{repo} /tmp/svn/#{repo}`
+			`svn checkout #{options} svn://localhost/#{repo} /tmp/svn/#{repo}`
 			if $?.exitstatus == 0
 
 				if web.nil?
--- lib/wire.rb
+++ lib/wire.rb
@@ -24,5 +24,5 @@ end
 # @author Bryan T. Meyers
 module Wire
 	# Current version of the Wire Gem
-	VERSION = '0.1.2.8'
+	VERSION = '0.1.2.9'
 end
