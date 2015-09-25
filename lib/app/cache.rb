dafe6313a2b12cf4e2b3c965f5bc6673862af19d
Bryan T. Meyers <btmeme@rit.edu>
2015-09-25 12:26:20 -0400

    FIXED: cache is working for complete lists, needs for for individual items

--- lib/app/cache.rb
+++ lib/app/cache.rb
@@ -8,43 +8,53 @@ module Cache
 		include Wire::Resource
 		extend Render
 
-		$cache = {}
-
 		def self.write_aware
 			$current_resource[:write_aware] = true
 		end
 
 		def self.update_cache(context)
+			ap $cache
 			action = (context.uri.length == 3) ? :readAll : :read
-			content = forward(action,context)
-			temp = context.uri.dup
-			temp.shift
-			target = $cache
-			until temp.empty?
-				current = temp.shift
-				unless target[current]
-					target[current] = {}
-				end
-				target = target[current]
+			action = ((context.uri.length == 4) and context.uri[3].eql?('new')) ? :readAll : action
+			if action.eql? :read
+				content = forward(action,context)
 			end
-			target = (context.uri.length == 3) ? target[:all] : target
-			target = content
+			all = forward( :readAll , context)
+			target = $cache
+			context.uri.each_with_index do |current,i|
+				unless i == 0 
+					puts i
+					unless target[current]
+						target[current] = {}
+					end
+					target = target[current]
+					if i == 2
+						target[:all] = all
+					end
+				end
+			end
+			if action.eql? :read
+				content
+			else
+				all
+			end
 		end
 
 		def self.get_cached(context)
-			temp = context.uri.dup
-			temp.shift
 			result = $cache
-			until temp.empty?
-				result = result[temp.shift]
+			context.uri[1..(context.uri.length-1)].each do |current|
+				if result
+					result = result[current]
+				end
 			end
-			if context.uri.length == 3
+			if result and (context.uri.length == 3)
 				result = result[:all]
 			end
 			result
 		end
 
 		def self.invoke(actions,context)
+			begin
 			case context.action
 				when :create,:update,:delete
 					result = forward(context.action,context)
@@ -58,6 +68,9 @@ module Cache
 						cached = update_cache(context)
 					end
 					cached
+			end
+			rescue RestClient::ResourceNotFound
+				404
 			end
 		end
 	end
